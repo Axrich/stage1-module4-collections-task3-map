5f340b140141f168009f959f0548edc75e7a81d2
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-16 13:07:21 +0500

    Update FunctionValueFinder.java

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,30 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        boolean isValuePresent = false;
+        for (Map.Entry<Integer, Integer> entry : functionMap.entrySet()) {
+            int functionValue = entry.getValue();
+            if (functionValue == requiredValue) {
+                isValuePresent = true;
+                break;
+            }
+        }
+        return isValuePresent;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> functionMap = new HashMap<>();
+        for (Integer key : sourceList) {
+            Integer value = 5 * key + 2;
+            functionMap.put(key, value);
+        }
+
+        return functionMap;
     }
 }
