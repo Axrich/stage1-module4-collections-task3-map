e8472ec7149203a770a8b8781a1549ad67f826b6
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-16 13:07:45 +0500

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+
+        String[] words = sentence.toLowerCase().split(" ");
+
+        for (String word : words) {
+
+            word = word.replaceAll("[,.]", "");
+
+            if (wordRepetitionMap.containsKey(word) ) {
+                wordRepetitionMap.put(word, wordRepetitionMap.get(word)+1);
+            }
+            else if (!word.isEmpty())
+            {wordRepetitionMap.put(word,1);}
+        }
+        return wordRepetitionMap;
     }
 }
+
